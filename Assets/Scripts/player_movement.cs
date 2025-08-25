f5e5f2cc78273c72323cec4c65e353c807c3956f
Parado-x-afk <xpara1323@gmail.com>
2025-08-25 22:00:12 +0530

    Few Movement Changes

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class player_movement : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] public float speed = 5f;
+    [SerializeField] public float jumpForce = 8f;
+
     private Rigidbody2D body;
-    public bool grounded;
-    public float horizontalInput;
     private Animator anim;
-    public GameObject win;
-    public bool isKnockedBack = false;
 
-    private float attackCooldown = 0.3f; // seconds between attacks
-    private float lastAttackTime;
+    public bool grounded;
 
-    private bool isAttacking;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -23,23 +18,15 @@ public class player_movement : MonoBehaviour
 
     private void Update()
     {
-        if (!isKnockedBack)
-        {
-            body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        // Movement only if not in attack (optional)
         body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
 
-        // Jumping
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && grounded)
         {
             jump();
         }
 
-        // Sprite facing direction
         if (horizontalInput > 0.0f)
         {
             transform.localScale = new Vector3(4, 4, 1);
@@ -49,88 +36,22 @@ public class player_movement : MonoBehaviour
             transform.localScale = new Vector3(-4, 4, 1);
         }
 
-        // Attempt Attack
-        if (Input.GetKeyDown(KeyCode.RightShift))
-        {
-            TryAttack();
-        }
-
-        // Animator states
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", grounded);
     }
 
-    // Called by animation event
-    public void StartAttack()
-    {
-        isAttacking = true;
-    }
-
-    // Called by animation event
-    public void EndAttack()
-    {
-        isAttacking = false;
-    }
-
-    private void TryAttack()
-    {
-        if (Time.time - lastAttackTime >= attackCooldown && canAttack())
-        {
-            anim.SetTrigger("attack");
-            lastAttackTime = Time.time;
-        }
-    }
-
     private void jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x,8);
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
         anim.SetTrigger("J");
         grounded = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (isAttacking && other.CompareTag("arrow"))
-        {
-            Destroy(other.gameObject);
-        }
-    }
-
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("arrow"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             grounded = true;
         }
-        if (collision.gameObject.CompareTag("temple"))
-        {
-            winf();
-        }
-    }
-
-    // Can only attack when standing still on ground
-    public bool canAttack()
-    {
-        return grounded && Mathf.Abs(horizontalInput) == 0;
-    }
-
-    public void restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public void quit()
-    {
-        SceneManager.LoadSceneAsync(0);
-    }
-
-    public void winf()
-    {
-        win.SetActive(true);
-        speed = 0f;
-        anim.SetBool("run", false);
-        body.linearVelocity = Vector2.zero;
-        enabled = false;
     }
 }
